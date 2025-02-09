c48acda63bd8147dbea32eef9ca95835a4cab7c5
Quan-TheDoctor <quantahs180461@fpt.edu.vn>
2025-02-09 16:57:16 +0700

    2/9/2025 4:51PM: - Added tailwindcss

--- src/main/java/com/se1873/js/springboot/project/repository/EmployeeRepository.java
+++ src/main/java/com/se1873/js/springboot/project/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
 package com.se1873.js.springboot.project.repository;
 
-import com.se1873.js.springboot.management.entity.Employee;
+import com.se1873.js.springboot.project.entity.Employee;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
