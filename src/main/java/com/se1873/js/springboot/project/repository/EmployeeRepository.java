990ea4727191b06b9ae7b2f32a611be1146210c6
hiuzz24 <hieupthe181180@fpt.edu.vn>
2025-02-10 21:52:10 +0700

    10/02/2025 9:52PM: - Add sort

--- src/main/java/com/se1873/js/springboot/project/repository/EmployeeRepository.java
+++ src/main/java/com/se1873/js/springboot/project/repository/EmployeeRepository.java
@@ -2,12 +2,25 @@ package com.se1873.js.springboot.project.repository;
 
 import com.se1873.js.springboot.project.dto.EmployeeDTO;
 import com.se1873.js.springboot.project.entity.Employee;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
   Employee findByEmployeeId(Integer employeeId);
 
   Employee findByEmployeeCode(String employeeCode);
+  @Query("SELECT e FROM Employee e " +
+          "JOIN EmploymentHistory eh ON eh.employee = e " +
+          "JOIN Department d ON d = eh.department " +
+          "WHERE eh.isCurrent = true " +
+          "ORDER BY d.departmentName ASC")
+
+  List<Employee> sortByDepartment(Pageable pageable);
 }
