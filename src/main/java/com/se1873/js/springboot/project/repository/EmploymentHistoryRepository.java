e9f93ab86ad68fafb5a890d526fd68e099178ddc
hiuzz24 <hieupthe181180@fpt.edu.vn>
2025-02-10 17:27:31 +0700

    10/02/2025 5:27PM: - Add filter EmployeeController.java

--- src/main/java/com/se1873/js/springboot/project/repository/EmploymentHistoryRepository.java
+++ src/main/java/com/se1873/js/springboot/project/repository/EmploymentHistoryRepository.java
@@ -2,9 +2,16 @@ package com.se1873.js.springboot.project.repository;
 
 import com.se1873.js.springboot.project.entity.EmploymentHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmploymentHistoryRepository extends JpaRepository<EmploymentHistory, Long> {
   EmploymentHistory findEmploymentHistoryByIsCurrentAndEmployee_EmployeeId(Boolean isCurrent, Integer employeeEmployeeId);
+
+  @Query("from EmploymentHistory eh where eh.department.departmentId = :departmentId and eh.isCurrent = true")
+  List<EmploymentHistory> finds(@Param("departmentId") Integer departmentId);
 }
