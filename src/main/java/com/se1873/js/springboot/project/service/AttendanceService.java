93dceea499de1cc3208878a92408b85b166837ec
DucAnh-sys <anhbldhe181672@fpt.edu.vn>
2025-02-10 18:25:44 +0700

    10/02/2025 6:24PM: - Add AttendanceController and related Repository and Services

---
+++ src/main/java/com/se1873/js/springboot/project/service/AttendanceService.java
@@ -0,0 +1,56 @@
+package com.se1873.js.springboot.project.service;
+
+import com.se1873.js.springboot.project.dto.AttendanceDTO;
+import com.se1873.js.springboot.project.dto.EmployeeDTO;
+import com.se1873.js.springboot.project.entity.Attendance;
+import com.se1873.js.springboot.project.entity.Employee;
+import com.se1873.js.springboot.project.repository.AttendanceRepository;
+import com.se1873.js.springboot.project.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AttendanceService {
+
+    private final AttendanceRepository attendanceRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public List<AttendanceDTO> getAttendanceByDate(LocalDate date) {
+        List<Attendance> attendances = new ArrayList<>();
+        attendances = attendanceRepository.findAttendancesByDate(date);
+        AttendanceDTO attendanceDTO = null;
+        List<AttendanceDTO> attendanceDTOs = new ArrayList<>();
+        for (Attendance attendance : attendances) {
+            Employee employee = attendance.getEmployee();
+            attendanceDTO = AttendanceDTO.builder()
+                    .employee(employee).attendance(attendance).build();
+            log.info(attendanceDTO.toString());
+
+            attendanceDTOs.add(attendanceDTO);
+        }
+        return attendanceDTOs;
+    }
+
+    public List<AttendanceDTO> getAllAttendance() {
+        List<Attendance> attendances = attendanceRepository.findAll();
+        log.info(attendances.toString());
+        List<AttendanceDTO> attendanceDTOs = new ArrayList<>();
+        for (Attendance attendance : attendances) {
+            Employee employee = attendance.getEmployee();
+            AttendanceDTO attendanceDTO = AttendanceDTO.builder()
+                    .employee(employee).attendance(attendance).build();
+
+            attendanceDTOs.add(attendanceDTO);
+        }
+        return attendanceDTOs;
+    }
+
+
+}
