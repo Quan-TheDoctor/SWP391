066af0595a1244efac4a7636f746292e52dbe7ef
Quan-TheDoctor <quantahs180461@fpt.edu.vn>
2025-02-10 16:12:11 +0700

    10/02/2025 4:09PM: - Added Insert Employee, View Employee and Employee List

--- src/main/java/com/se1873/js/springboot/project/controller/EmployeeController.java
+++ src/main/java/com/se1873/js/springboot/project/controller/EmployeeController.java
@@ -1,40 +1,78 @@
 package com.se1873.js.springboot.project.controller;
 
-import com.se1873.js.springboot.management.entity.Employee;
-import com.se1873.js.springboot.management.service.EmployeeService;
+import com.se1873.js.springboot.project.dto.EmployeeDTO;
+import com.se1873.js.springboot.project.entity.Department;
+import com.se1873.js.springboot.project.entity.Employee;
+import com.se1873.js.springboot.project.entity.Position;
+import com.se1873.js.springboot.project.repository.ContractRepository;
+import com.se1873.js.springboot.project.service.DepartmentService;
+import com.se1873.js.springboot.project.service.EmployeeService;
+import com.se1873.js.springboot.project.service.PositionService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-@RequestMapping("/employee")
-@RequiredArgsConstructor
-public class EmployeeController {
+import java.util.ArrayList;
+import java.util.List;
 
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/employee")
+public class EmployeeController {
+  private final DepartmentService departmentService;
+  private final PositionService positionService;
   private final EmployeeService employeeService;
+  private final ContractRepository contractRepository;
+  private List<Department> departments;
+  private List<Position> positions;
+
+  @PostConstruct
+  public void init() {
+    departments = departmentService.getAllDepartments();
+    positions = positionService.getAllPositions();
+  }
 
   @RequestMapping
-  public String index(Model model) {
-    model.addAttribute("employees", employeeService.findAllEmployees());
-    model.addAttribute("page", "fragments/employee");
-    model.addAttribute("header", "fragments/header");
-    return "index";
+  public String employee(Model model) {
+    log.info("[EMPLOYEE] - employee page");
+
+    List<EmployeeDTO> employees = employeeService.getAllEmployees();
+
+    model.addAttribute("employees", employees);
+    model.addAttribute("departments", departments);
+    model.addAttribute("positions", positions);
+    return "employee";
   }
 
   @RequestMapping("/view")
   public String view(Model model,
-                     @RequestParam("employeeId") Integer employeeId,
-                     @RequestParam(value = "tab", required = false, defaultValue = "personal") String tab,
-                     @RequestParam(value = "edit", required = false) Boolean edit) {
+                     @RequestParam("employeeId") Integer employeeId) {
+    EmployeeDTO employeeDTO = employeeService.getEmployeeByEmployeeId(employeeId);
+    model.addAttribute("employee", employeeDTO);
+    return "employee-details";
+  }
 
-    Employee employee = employeeService.findEmployeeByEmployeeId(employeeId);
-    model.addAttribute("employee", employee);
-    model.addAttribute("tab", tab);
-    model.addAttribute("edit", edit);
-    model.addAttribute("page", "fragments/employee-detail");
-    model.addAttribute("header", "fragments/header");
-    return "index";
+  @RequestMapping("/employee-insert")
+  public String viewInsert(Model model) {
+    log.info("[EMPLOYEE-INSERT] - employee insert page");
+    model.addAttribute("departments", departments);
+    model.addAttribute("positions", positions);
+    model.addAttribute("employee", new EmployeeDTO());
+    model.addAttribute("countContract", contractRepository.count());
+    return "employee-insert";
+  }
+
+  @RequestMapping("/insert")
+  public String insert(Model model,
+                       @ModelAttribute("employee") EmployeeDTO employeeDTO) {
+    log.info("[INSERT] - employee insert");
+    employeeService.insertEmployee(employeeDTO);
+    return "redirect:/employee";
   }
 }
--- src/main/java/com/se1873/js/springboot/project/service/EmployeeService.java
+++ src/main/java/com/se1873/js/springboot/project/service/EmployeeService.java
@@ -1,41 +1,125 @@
 package com.se1873.js.springboot.project.service;
 
-import com.se1873.js.springboot.management.entity.Employee;
-import com.se1873.js.springboot.management.repository.EmployeeRepository;
+import com.se1873.js.springboot.project.dto.EmployeeDTO;
+import com.se1873.js.springboot.project.entity.*;
+import com.se1873.js.springboot.project.repository.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
+@Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class EmployeeService {
 
   private final EmployeeRepository employeeRepository;
+  private final EmploymentHistoryRepository employmentHistoryRepository;
+  private final DepartmentRepository departmentRepository;
+  private final PositionRepository positionRepository;
+  private final UserRepository userRepository;
+  private final ContractRepository contractRepository;
 
-  @Transactional
-  public Employee findEmployeeByEmployeeId(Integer employeeId) {
-    Employee employee = employeeRepository.findEmployeeByEmployeeId(employeeId);
-    employee.getDependents();
-    employee.getContracts();
-    employee.getEmployeeSkills();
-    employee.getEmploymentHistories();
-    employee.getQualifications();
-    return employee;
+  public EmployeeDTO getEmployeeByEmployeeId(Integer employeeId) {
+    return convertEmployeeToEmployeeDTO(employeeRepository.findByEmployeeId(employeeId));
   }
 
-  public Set<Employee> findAllEmployees() {
-    List<Employee> employeesWithHistories = employeeRepository.findAllWithEmployementHistories(Sort.by(Sort.Direction.ASC, "employeeId"));
-    List<Employee> employeesWithContracts = employeeRepository.findAllWithContracts(Sort.by(Sort.Direction.ASC, "employeeId"));
+  public List<EmployeeDTO> getAllEmployees() {
+    return employeeRepository.findAll(Sort.by(Sort.Direction.ASC, "employeeId"))
+      .stream()
+      .map(this::convertEmployeeToEmployeeDTO)
+      .toList();
+  }
 
-    Set<Employee> employees = new HashSet<>();
-    employees.addAll(employeesWithHistories);
-    employees.addAll(employeesWithContracts);
-    return employees;
+  @Transactional
+  public void insertEmployee(EmployeeDTO employeeDTO) {
+    try {
+      log.info("1");
+
+      Employee saveEmployee = employeeDTO.getEmployee();
+      if (saveEmployee.getEmployeeId() != null) {
+        saveEmployee.setEmployeeId(null);
+      }
+      log.info("Employee before save: {}", saveEmployee);
+      log.info("employeeCode: {}", saveEmployee.getEmployeeCode());
+      log.info("idNumber: {}", saveEmployee.getIdNumber());
+      log.info("companyEmail: {}", saveEmployee.getCompanyEmail());
+      employeeRepository.save(saveEmployee);
+      log.info("2");
+      Employee employee = employeeRepository.findByEmployeeCode(employeeDTO.getEmployee().getEmployeeCode());
+      log.info("3");
+      Department department = departmentRepository.findByDepartmentCode(employeeDTO.getEmploymentHistory().getDepartment().getDepartmentCode());
+      log.info("4");
+      Position position = positionRepository.findByPositionCode(employeeDTO.getEmploymentHistory().getPosition().getPositionCode());
+      log.info("5");
+      EmploymentHistory employmentHistory = employeeDTO.getEmploymentHistory();
+      employmentHistory.setEmployee(employee);
+      employmentHistory.setDepartment(department);
+      employmentHistory.setPosition(position);
+      employmentHistory.setIsCurrent(true);
+      Contract contract = employeeDTO.getContract();
+      contract.setEmployee(employee);
+      contract.setPresent(true);
+
+      BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+      String hashedPassword = passwordEncoder.encode("test");
+
+      User user = User.builder()
+        .createdAt(LocalDateTime.now())
+        .employee(employee)
+        .passwordHash(hashedPassword)
+        .username(employeeDTO.getEmployee().getCompanyEmail())
+        .build();
+
+      contractRepository.save(contract);
+      userRepository.save(user);
+      employmentHistoryRepository.save(employmentHistory);
+
+      log.info(employee.getEmployeeId().toString());
+      log.info(department.getDepartmentId().toString());
+      log.info(position.getPositionId().toString());
+
+    } catch (DataIntegrityViolationException e) {
+      log.error("Employee with ID {} already exists. Error: {}", employeeDTO.getEmployee().getEmployeeId(), e.getMessage());
+      throw new DataIntegrityViolationException(employeeDTO.toString());
+    }
+    //employeeRepository.saveAndFlush(employeeDTO.getEmployee());
+  }
+
+  private EmployeeDTO convertEmployeeToEmployeeDTO(Employee employee) {
+    EmploymentHistory employmentHistory = null;
+    Department department = null;
+    Position position = null;
+    Contract contract = null;
+
+    for(var emp : employee.getEmploymentHistories()) {
+      if(emp.getIsCurrent()) {
+        employmentHistory = emp;
+        department = emp.getDepartment();
+        position = emp.getPosition();
+      }
+    }
+
+    for(var cont : employee.getContracts()) {
+      if(cont.isPresent()) {
+        contract = cont;
+      }
+    }
+
+    return EmployeeDTO.builder()
+      .employee(employee)
+      .employmentHistory(employmentHistory)
+      .employmentHistories(employee.getEmploymentHistories())
+      .qualifications(employee.getQualifications())
+      .department(department)
+      .position(position)
+      .contract(contract)
+      .build();
   }
 }
