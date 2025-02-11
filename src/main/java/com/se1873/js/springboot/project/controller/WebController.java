e2043a613830f1a2ec3a994651e3093a00a5eb3c
ddttu <148530352+ddttu@users.noreply.github.com>
2025-02-11 23:22:36 +0700

    Commit login feature

---
+++ src/main/java/com/se1873/js/springboot/project/controller/WebController.java
@@ -0,0 +1,29 @@
+package com.se1873.js.springboot.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String home() {
+        return "home"; // Trả về home.html
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard"; // Trả về dashboard.html (chỉ truy cập khi đăng nhập)
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("errorMsg", "Tài khoản hoặc mật khẩu không đúng!");
+        }
+        if (logout != null) {
+            model.addAttribute("logoutMsg", "Bạn đã đăng xuất thành công!");
+        }
+        return "login"; // Trả về login.html
+    }
+}
