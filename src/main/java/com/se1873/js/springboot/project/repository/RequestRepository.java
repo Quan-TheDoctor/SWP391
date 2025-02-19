f496fe559a28594134362a2c1dcc387740f588c6
ddttu <148530352+ddttu@users.noreply.github.com>
2025-02-19 21:37:24 +0700

    19/02/2025 search request

--- src/main/java/com/se1873/js/springboot/project/repository/RequestRepository.java
+++ src/main/java/com/se1873/js/springboot/project/repository/RequestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,7 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
     Page<Request> findByRequestType(String type, Pageable pageable);
     @Query("SELECT  r.requestType FROM Request r")
     List<String> findRequestTypes();
+    @Query("SELECT r FROM Request r WHERE LOWER(r.user.username) LIKE LOWER(CONCAT('%', :requesterName, '%'))")
+    Page<Request> searchRequestsByRequester(@Param("requesterName") String requesterName, Pageable pageable);
+
 }
